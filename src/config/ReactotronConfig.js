e7b356ef5d484dc4d377790712bb945165afb2e6
Tiago Felipe <tiagofsv95@gmail.com>
2020-03-06 16:52:48 -0300

    Commit until challenge

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({host: '10.0.3.2'})
+  const tron = Reactotron.configure({ host: '10.0.3.2' })
     .useReactNative()
     .connect();
 
