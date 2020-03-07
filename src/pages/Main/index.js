fadc57d22c068fb7c73c5cb3f06bde6f988e79d2
Tiago Felipe <tiagofsv95@gmail.com>
2020-03-07 14:45:34 -0300

    User validation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Keyboard, ActivityIndicator } from 'react-native';
+import { Keyboard, ActivityIndicator, Alert } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
@@ -63,22 +63,36 @@ export default class Main extends Component {
 
     this.setState({ loading: true });
 
-    const response = await api.get(`/users/${newUser}`);
+    try {
+      const response = await api.get(`/users/${newUser}`);
 
-    const data = {
-      name: response.data.name,
-      login: response.data.login,
-      bio: response.data.bio,
-      avatar: response.data.avatar_url,
-    };
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
 
-    this.setState({
-      users: [...users, data],
-      newUser: '',
-      loading: false,
-    });
+      this.setState({
+        users: [...users, data],
+        newUser: '',
+        loading: false,
+      });
 
-    Keyboard.dismiss();
+      Keyboard.dismiss();
+    } catch (error) {
+      this.setState({
+        newUser: '',
+        loading: false,
+      });
+
+      Alert.alert(
+        'Error',
+        'Erro ao buscar usuário.\nPor favor, insira um usuário válido.',
+        [{ text: 'OK', onPress: () => {} }],
+        { cancelable: false },
+      );
+    }
   };
 
   handleNavigate = user => {
